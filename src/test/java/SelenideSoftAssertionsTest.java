d4175baf265633f95bac6655b76c8e76172c9981
Ekaterina Vodolazhskaia <katik_v@inbox.ru>
2024-04-24 18:30:07 +0300

    added SelenideSoftAssertionsTest

---
+++ src/test/java/SelenideSoftAssertionsTest.java
@@ -0,0 +1,42 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideSoftAssertionsTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void shouldHaveJUnit5Example() {
+
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-box").$(withTagAndText("button","Show 3 more pages")).click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+                                """));
+    }
+
+}
